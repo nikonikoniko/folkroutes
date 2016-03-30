820087d05d1af9a91adb63e0ae1b77d5d68b97d8
Niko <niko@niko.io>
2016-03-30 16:35:13 +0200

    changed

--- constellation/migrations/0007_merge.py
+++ constellation/migrations/0007_merge.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('constellation', '0005_auto_20151115_2044'),
         ('constellation', '0006_auto_20151115_2129'),
     ]
 
