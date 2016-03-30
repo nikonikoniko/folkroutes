54aeb0c822d5d127ad22293b977b9b911cc24c35
Niko <niko@niko.io>
2016-03-30 16:15:49 +0200

    adjusted settings

--- folkroutes/settings.py
+++ folkroutes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import folkroutes.local_settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,12 +80,6 @@ WSGI_APPLICATION = 'folkroutes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
-    }
-}
 
 
 # Internationalization
